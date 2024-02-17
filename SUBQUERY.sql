156fd78ec1d1e8a3c34af609c3163b0f97e40bcf
Mike Zhang <70972570+fistfulofyen@users.noreply.github.com>
2024-02-17 14:23:09 -0500

    Update SUBQUERY.sql

--- SUBQUERY.sql
+++ SUBQUERY.sql
@@ -29,3 +29,22 @@ WHERE
   ) 
 ORDER BY 
   city;
+
+-- film
+SELECT 
+  film_id, 
+  title 
+FROM 
+  film 
+WHERE 
+  film_id IN (
+    SELECT 
+      film_id 
+    FROM 
+      film_category 
+      INNER JOIN category USING(category_id) 
+    WHERE 
+      name = 'Action'
+  ) 
+ORDER BY 
+  film_id;
